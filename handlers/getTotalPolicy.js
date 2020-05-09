a1824a73008b208bbfbdad414e4ac97b34a1f6d9
Jorge Loaiza <jloaiza@acid.cl>
2020-05-09 16:07:14 -0400

    fix(jl, getTotalPolicy): Refactor of the service and add new class Person

--- handlers/getTotalPolicy.js
+++ handlers/getTotalPolicy.js
@@ -1,50 +1,6 @@
 import axios from 'axios';
-
-/**
- * Calculate health life policy cost by one worker
- * @param {object} person
- * @param {number} percent
- */
-const calculatehealthLifeCost = (person, percent) => {
-  let costUF = 0;
-  let calculatePercentage = 0;
-
-  if (person.childs === 1) {
-    calculatePercentage = percent * 0.4396;
-    costUF = calculatePercentage / 100;
-  } else if (person.childs > 1) {
-    calculatePercentage = percent * 0.5599;
-    costUF = calculatePercentage / 100;
-  } else {
-    calculatePercentage = percent * 0.279;
-    costUF = calculatePercentage / 100;
-  }
-
-  return costUF;
-};
-
-/**
- * Calculate dental policy cost by one worker
- * @param {object} person
- * @param {number} percent
- */
-const calculateDentalCost = (person, percent) => {
-  let costUF = 0;
-  let calculatePercentage = 0;
-
-  if (person.childs === 1) {
-    calculatePercentage = percent * 0.195;
-    costUF = calculatePercentage / 100;
-  } else if (person.childs > 1) {
-    calculatePercentage = percent * 0.248;
-    costUF = calculatePercentage / 100;
-  } else {
-    calculatePercentage = percent * 0.12;
-    costUF = calculatePercentage / 100;
-  }
-
-  return costUF;
-};
+import Person from '../lib/Person';
+const UF_VALUE = 28716.52;
 
 export const handler = (event, context, cb) => {
   return axios
@@ -52,29 +8,28 @@ export const handler = (event, context, cb) => {
     .then((response) => {
       if (!!response.data && !!response.data.policy) {
         const workers = response.data.policy.workers;
-        const percentCovered = response.data.policy.company_percentage;
+        const percent = response.data.policy.company_percentage;
         let totalCostCLP = 0;
         let totalCostUF = 0;
 
         // Add to all workers the healthLifeCost and dentalPolicyCost
         const resultWorkers = workers.map((person) => {
-          person.healthLifeCost = calculatehealthLifeCost(
-            person,
-            percentCovered
-          );
-          person.dentalPolicyCost = calculateDentalCost(person, percentCovered);
+          const personObject = new Person(person); // new instance of person
 
-          return person;
+          return {
+            ...personObject,
+            healthLife: personObject.calculatePolicyCost('health', percent),
+            dental: personObject.calculatePolicyCost('dental', percent),
+          };
         });
 
         // Calculate total cost of all workers in uf
         for (var i in resultWorkers) {
-          totalCostUF +=
-            resultWorkers[i].healthLifeCost + resultWorkers[i].dentalPolicyCost;
+          totalCostUF += resultWorkers[i].healthLife + resultWorkers[i].dental;
         }
 
         // Calculate total cost of all workers in clp
-        totalCostCLP = totalCostUF * 28716.52;
+        totalCostCLP = totalCostUF * UF_VALUE;
 
         return cb(null, {
           statusCode: 200,
@@ -82,7 +37,7 @@ export const handler = (event, context, cb) => {
             'Content-Type': 'application/json;charset=utf-8',
           },
           data: {
-            resultWorkers,
+            workers: resultWorkers,
             totalCostUF,
             totalCostCLP,
           },
